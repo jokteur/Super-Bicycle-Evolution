d3b76eacf03cd7d28396853f3c4df20527a4711b
Unknown <kolaru@hotmail.com>
2017-08-23 22:00:32 +0200

    Working python prototype

--- python/creature.py
+++ python/creature.py
@@ -1,40 +1,62 @@
-from scheduler import Scheduler
-from universe import Universe
-
 class Creature:
     _hp = 0
     _neural_network = None
+    _id = 0
 
-    def __init__(self):
+    def __init__(self, universe):
+        self.universe = universe
+        self._id = Creature._id
+        Creature._id += 1
+
         self._hp = 10
-        self._attack = 1
-        self._neural_network = NeuralNetwork()
+        self.attack = 1
+        self._neural_network = NeuralNetwork(self)
         self._next_action = Wait()
+
+    def __repr__(self):
+        return "Creature {} [hp = {}]".format(self._id, self._hp)
 
     def act(self):
         self._next_action.resolve()
         self._next_action, dtime = self._make_decision()
-        Scheduler.register_event(creature=self, dtime=dtime)
+        if self._next_action is not None:
+            self.universe.schedule.register_event(creature=self, dtime=dtime)
 
     def lose_hp(self, hp):
+        print("{} suffers".format(self))
         self._hp -= hp
         if self._hp < 0:
             self._die()
 
     def _make_decision(self):
-        external_inputs = Universe.get_vision_of(self)
+        external_inputs = self.universe.get_vision_of(self)
         return self._neural_network.process(external_inputs)
 
+    def _die(self):
+        print("{} dies".format(self))
+        self.universe.remove(self)
+
 class NeuralNetwork:
-    def process(external_inputs):
-        return Attack(external_inputs[0]), 10
+    def __init__(self, owner):
+        self.owner = owner
+    def process(self, external_inputs):
+        if external_inputs is not None:
+            return Attack(attacker=self.owner,
+                          target=external_inputs[0]), 10
+        else:
+            return None, 0
 
 class Action:
-    pass
-
-class Attack(Action):
-    pass
-
-class Wait(Action):
     def resolve(self):
         pass
+
+class Attack(Action):
+    def __init__(self, attacker=None, target=None):
+        self.attacker = attacker
+        self.target = target
+
+    def resolve(self):
+        self.target.lose_hp(self.attacker.attack)
+
+class Wait(Action):
+    pass
--- python/universe.py
+++ python/universe.py
@@ -1,16 +1,36 @@
-from scheduler import Scheduler
+from schedule import Schedule
+from creature import Creature
+
+MAX_CURRENT_TIME = 100
 
 class Universe:
     creatures = []
     def __init__(self):
-        self.creatures.extend([Creature(), Creature()])
+        self.creatures.extend([Creature(self), Creature(self)])
+        self.schedule = Schedule()
         timer = 0
         for crea in self.creatures:
-            Scheduler.register_event(creature=crea, dtime=timer)
+            self.schedule.register_event(creature=crea, dtime=timer)
             timer += 1
 
-    @classmethod
-    def get_vision_of(cls, creature):
-        for crea in cls.creatures:
+    def get_vision_of(self, creature):
+        for crea in self.creatures:
             if crea != creature:
                 return [crea]
+        return None
+
+    def remove(self, creature):
+        self.creatures.remove(creature)
+        self.schedule.remove(creature)
+
+    def run(self):
+        while len(self.schedule) > 0 and self.schedule.current_time < MAX_CURRENT_TIME:
+            print(self.schedule)
+            current_creature = self.schedule.next_creature()
+            current_creature.act()  # Prochaine action dans l'event ou dans la créature ?
+
+
+if __name__ == '__main__':
+    universe = Universe()
+    universe.run()
+    print("Exited normally")
